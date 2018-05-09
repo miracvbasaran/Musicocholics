364561383d2892baf86b889770459b697eb45175
Metehan Kaya <kayahanmete96@gmail.com>
2018-05-09 14:26:17 +0300

    Message List v1 is added

---
+++ message_list.php
@@ -0,0 +1,88 @@
+<?php
+
+	include("session.php");
+
+    $uid = mysqli_real_escape_string($db,$_GET['login_id']);
+    $query = "SELECT * FROM user WHERE user_id = '$uid' ";
+    $result = mysqli_query($db, $query);
+    $user_array = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+    if (isset($_POST['sendmessage_button'])) {
+    	header("location: send_message.php?");
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Musicholics - My Profile</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="#">Home</a></li>      
+	      <li><a href="own_profile.php">Profile</a></li>
+	      <li><a href="view_playlists.php">Playlist</a></li>
+	      <li><a href="view_tracks.php">Tracks</a></li>
+		<li><a href="friends_list.php">Friends</a></li>
+		<li><a href="message_list.php">Messages</a></li>
+		<li><a href="search_result_screen.php">Search</a></li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	      <li><a href="change_general_information.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
+	      <li><a href="homepage.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+	    </ul>
+	  </div>
+	</nav>
+
+	<div class="container">
+		<p>
+			<input id='Submit' name='sendmessage_button' value='Submit' type='button' value='SEND MESSAGE'>
+		</p>
+	</div>
+
+	<div class="container">
+		<p> MESSAGES <br> <br>
+			<?php
+				$query = "SELECT U.fullname , M.message FROM User U , SENDS_MESSAGE M WHERE M.receiver_id = '$uid' AND M.sender_id = U.user_id ORDER BY M.date DESC";
+				$result = mysqli_query($db, $query);
+				$query2 = "SELECT COUNT(*) FROM User U , SENDS_MESSAGE M WHERE M.receiver_id = '$uid' AND M.sender_id = U.user_id";
+				$result2 = mysqli_query($db, $query2);
+				printf( "Total messages: %d\n" , $result2 );
+				while ($row = mysqli_fetch_array($result, MYSQL_NUM)) {
+					printf( "%s\n%s\n" , $row[0] , $row[1] );
+				}
+			?>
+		</p>
+	</div>
+
+	<div> 	
+		<footer>
+			<?php
+				$query = "SELECT L1.track_id FROM listens L1 WHERE L1.user_id = '$uid' AND 
+					date = SELECT max(L2.date) FROM listens L2 WHERE L2.user_id = '$uid'";
+				$result = mysqli_query($db, $query);
+				$query2 = "SELECT track_name,duration FROM track WHERE track_id = $result";
+				$result2 = mysqli_query($db, $query2);
+				$track_array = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+			    $track_name = $track_array['track_name'];
+			    $duration = $track_array['duration'];
+				echo $track_name;
+				echo $duration;
+			?>
+		</footer>
+	</div>
+
+</body>
+
+</html>
