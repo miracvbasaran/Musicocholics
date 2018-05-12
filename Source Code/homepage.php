162c1e78bc3264ff247336bfb7822a08013fbd7f
Esra Ayaz <esranayaz@gmail.com>
2018-05-13 00:08:18 +0300

    Add files via upload

--- Source Code/homepage.php
+++ Source Code/homepage.php
@@ -1,29 +1,22 @@
 <?php 
-	include("session.php");
-	include("connection.php");
+include("session.php");
+include("connection.php");
 ?>
 
 <!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<title>Musicholics - Homepage</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Sign In</a></li>
-					<br/><br/><br/>
-					<li><a href="register.php"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a></li>
-				</ul>
-			</div>
-		</nav>
-	</body>
+<html lang="en">
+<head>
+	<title>Musicholics - Homepage</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div align = "center">
+		<tr><td><a href='login.php'>Sign In</a></td></tr>
+		<tr><td><a href='register.php'>Sign Up</a></td></tr>
+	</div>
+</body>
 </html>
