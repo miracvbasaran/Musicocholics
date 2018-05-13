8cab1ebd4ec9942bb2c9bc2ab9259ac41f566023
Esra Ayaz <esranayaz@gmail.com>
2018-05-13 23:39:36 +0300

    Update advanced_artist_search.php

--- Source Code/advanced_artist_search.php
+++ Source Code/advanced_artist_search.php
@@ -1,12 +1,18 @@
-<?php 
-include("session.php");
+<?php
+
+	include("session.php");
+
+    $uid = mysqli_real_escape_string($db,$_SESSION['login_id']);
+    $query1 = "SELECT * FROM user WHERE user_id = '$uid' ";
+    $result1 = mysqli_query($db, $query1);
+    $user_array = mysqli_fetch_array($result1, MYSQLI_ASSOC);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Musicholics - Advanced Album Search</title>
+	<title>Musicholics - Advanced Track Search</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -15,37 +21,35 @@ include("session.php");
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-      
-      <li><a href="own_profile.php">Profile</a></li>
-      <li><a href="view_playlists.php">Playlist</a></li>
-      <li><a href="view_tracks.php">Tracks</a></li>
-  <li><a href="friends.php">Friends</a></li>
-  <li><a href="message_list.php">Messages</a></li>
-  <li><a href="search.php">Search</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="change_general_information.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
-      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-  </div>
-</nav>
-		
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<ul class="nav navbar-nav">
+				<li><a href="own_profile.php">Profile</a></li>
+				<li><a href="playlists.php">Playlist</a></li>
+				<li><a href="view_tracks.php">Tracks</a></li>
+				<li><a href="friends.php">Friends</a></li>
+				<li><a href="message_list.php">Messages</a></li>
+				<li class="active"><a href="search.php">Search</a></li>
+			</ul>
+		    <ul class="nav navbar-nav navbar-right">
+				<li><a href="change_general_information.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
+				<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+		    </ul>
+		</div>
 		
 		<div align = "center">
 			<form action = "#" method = "post" onsubmit = "return check()">
 				<font color="white">
-					<br/><br/><br/><br/>MUSICHOLICS<br/><br/>Advanced Album Search<br/><br/><br/><br/>
-					<input type = "text" name = "search_key" placeholder = "Album Name"> <br/><br/>
+					<br/><br/><br/><br/>MUSICHOLICS<br/><br/>Advanced Artist Search<br/><br/><br/><br/>
+					<font color="black">
+						<input type = "text" name = "search_key" placeholder = "Artist Name"> <br/><br/>
+					</font>
 					<input type = "radio" name="match" value="matches"/> Exactly matches &nbsp; &nbsp;
 					<input type = "radio" name="match" value="contains"/> Contains &nbsp; &nbsp;
 					<input type = "radio" name="match" value="starts_with"/> Starts with
 					<br/><br/>
-					<input type = "text" name = "description" placeholder = "Description contains.."> <br/><br/>
-					<br/><br/>
 					<font color="black">
+						<input type = "text" name = "description" placeholder = "Description contains.."> <br/><br/>
 						<input id = "" value = "Search" name = "search" type = "submit"> </button> <br/><br/>
 					</font>
 					<br/><br/>
@@ -57,14 +61,17 @@ include("session.php");
 		<script type = "text/javascript">
 			function check(){
 				var search_key = document.getElementById( "search_key").value;
-				var match = document.getElementById( "description").value;
+				var match = document.getElementById( "match").value;
+				var description = document.getElementById( "description").value;
 
 				if( search_key == "")
 					alert( "Please type an artist name");
 				if( match = "")
 					alert( "Please one of \"Exactly matches\", \"Exactly Contains\", \"Starts with\"" );
+				if( description = "")
+					alert( "Please type an description");
 				
-				location.href = "advanced_artist_search.php";
+				location.href = "advanced_album_search.php";
 			}
 			</script>
 		
@@ -76,25 +83,27 @@ include("session.php");
 			$match = mysqli_real_escape_string( $db, $_POST['match']);
 			$description = mysqli_real_escape_string( $db, $_POST['description']);
 			
+	
 			if( $match == "matches"){
 				$query = mysqli_query( $db, "SELECT * FROM Artist WHERE ( artist_name LIKE '$search_key') 
-																		AND description LIKE '$description'");
+																		AND description LIKE '%$description%';");
+										
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='artist.php?artist_id=".$row['artist_id']."'>.$row['artist_id'].</a></td> </tr>");
+					echo( "<tr> <td><a href='view_artist.php?artist_id=".$row['artist_id']."'>".$row['artist_name']."</a></td> </tr><br/>");
 				}
 			}
 			else if( $match == "contains"){
-				$query = mysqli_query( $db, "SELECT * FROM Artist WHERE ( artist_name LIKE '$search_key') 
-																		AND description LIKE '$description'");
+				$query = mysqli_query( $db, "SELECT * FROM Artist WHERE ( artist_name LIKE '%$search_key%') 
+																		AND description LIKE '%$description%';");
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='artist.php?artist_id=".$row['artist_id']."'>.$row['artist_id'].</a></td> </tr>");
+					echo( "<tr> <td><a href='view_artist.php?artist_id=".$row['artist_id']."'>".$row['artist_name']."</a></td> </tr><br/>");
 				}
 			}
 			else if( $match == "starts_with"){
 				$query = mysqli_query( $db, "SELECT * FROM Artist WHERE ( artist_name LIKE '$search_key') 
-																		AND description LIKE '$description'");
+																		AND description LIKE '%$description%';");
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='artist.php?artist_id=".$row['artist_id']."'>.$row['artist_id'].</a></td> </tr>");
+					echo( "<tr> <td><a href='view_artist.php?artist_id=".$row['artist_id']."'>".$row['artist_id']."</a></td> </tr>");
 				}
 			}
 			
@@ -103,7 +112,7 @@ include("session.php");
 		?>
 	
 		<br/><br/><br/>
-
+	</nav>
 
 	<div>
 	<footer>
@@ -125,3 +134,7 @@ include("session.php");
 	</div>
 </body>
 </html>
+
+
+
+
