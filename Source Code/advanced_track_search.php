e5125009f011cb6d7088609aed9065d20ed5cdc3
Esra Ayaz <esranayaz@gmail.com>
2018-05-13 19:59:21 +0300

    advanced_track_search done

--- Source Code/advanced_track_search.php
+++ Source Code/advanced_track_search.php
@@ -1,5 +1,11 @@
-<?php 
-include("session.php");
+<?php
+
+	include("session.php");
+
+    $uid = mysqli_real_escape_string($db,$_SESSION['login_id']);
+    $query1 = "SELECT * FROM user WHERE user_id = '$uid' ";
+    $result1 = mysqli_query($db, $query1);
+    $user_array = mysqli_fetch_array($result1, MYSQLI_ASSOC);
 
 ?>
 
@@ -15,45 +21,40 @@ include("session.php");
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-      
-      <li><a href="own_profile.php">Profile</a></li>
-      <li><a href="view_playlists.php">Playlist</a></li>
-      <li><a href="view_tracks.php">Tracks</a></li>
-  <li><a href="friends.php">Friends</a></li>
-  <li><a href="message_list.php">Messages</a></li>
-  <li><a href="search.php">Search</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="change_general_information.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
-      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-  </div>
-</nav>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<ul class="nav navbar-nav">
+				<li><a href="own_profile.php">Profile</a></li>
+				<li><a href="playlists.php">Playlist</a></li>
+				<li><a href="view_tracks.php">Tracks</a></li>
+				<li><a href="friends.php">Friends</a></li>
+				<li><a href="message_list.php">Messages</a></li>
+				<li class="active"><a href="search.php">Search</a></li>
+			</ul>
+		    <ul class="nav navbar-nav navbar-right">
+				<li><a href="change_general_information.php"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
+				<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+		    </ul>
+		</div>
 		
 		<div align = "center">
 			<form action = "#" method = "post" onsubmit = "return check()">
 				<font color="white">
 					<br/><br/><br/><br/>MUSICHOLICS<br/><br/>Advanced Track Search<br/><br/><br/><br/>
-					<input type = "text" name = "search_key" placeholder = "Track Name"> <br/><br/>
+					<font color="black">
+						<input type = "text" name = "search_key" placeholder = "Track Name"> <br/><br/>
+					</font>
 					<input type = "radio" name="match" value="matches"/> Exactly matches &nbsp; &nbsp;
 					<input type = "radio" name="match" value="contains"/> Contains &nbsp; &nbsp;
 					<input type = "radio" name="match" value="starts_with"/> Starts with
 					<br/><br/>
 					<font color="black">
 						<input type = "date" name = "from_date" placeholder = "Starting date for addition" > 
-						<input type = "date" name = "end_date" placeholder = "End date for addition"> 
+						<input type = "date" name = "end_date" placeholder = "End date for addition">
 					</font>
 					<br/><br/>
-					<font color="black">
-						<input type = "date" name = "min_duration" placeholder = "Minimum duration" > 
-						<input type = "date" name = "max_duration" placeholder = "Maximum duration"> 
-					</font>
-					<br/><br/>
-					<input type = "radio" name="type" value="Album"/> Album &nbsp; &nbsp;
-					<input type = "radio" name="type" value="Single"/> Single
+					<input type = "radio" name="type" value="Studio"/> Studio &nbsp; &nbsp;
+					<input type = "radio" name="type" value="Live"/> Live
 					<br/><br/>
 					<font color="black">
 						<input id = "" value = "Search" name = "search" type = "submit"> </button> <br/><br/>
@@ -70,8 +71,6 @@ include("session.php");
 				var match = document.getElementById( "match").value;
 				var from_date = document.getElementById( "from_date").value;
 				var end_date = document.getElementById( "end_date").value;
-				var min_duration = document.getElementById( "min_duration").value;
-				var max_duration = document.getElementById( "max_duration").value;
 				var type = document.getElementById( "type").value;
 
 				if( search_key == "")
@@ -82,10 +81,6 @@ include("session.php");
 					alert( "Please select a starting date");
 				if( end_date = "")
 					alert( "Please select a ending date");
-				if( min_duration = "")
-					alert( "Please type a minimum date");
-				if( max_duration = "")
-					alert( "Please type a maximum date");
 				if( type = "")
 					alert( "Please select album type");
 				
@@ -105,37 +100,35 @@ include("session.php");
 			$max_duration = mysqli_real_escape_string( $db, $_POST['max_duration']);
 			$type = mysqli_real_escape_string( $db, $_POST['type']);
 			
+			
+			
 			if( $match == "matches"){
 				$query = mysqli_query( $db, "SELECT * FROM Track WHERE ( track_name LIKE '$search_key') 
-																		AND album_type = '$type' 
-																		AND published_date >= '$from_date' 
-																		AND published_date <= '$end_date'
-																		AND published_date >= '$min_duration' 
-																		AND published_date <= '$max_duration';");
+																		AND recording_type = '$type' 
+																		AND date_of_addition >= '$from_date'
+																		AND date_of_addition <= '$end_date';");
+
+										
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='album.php?track_id=".$row['track_id']."'>.$row['track_id'].</a></td> </tr>");
+																			echo( "<tr> <td><a href='track.php?track_id=".$row['track_id']."'>".$row['track_name']."</a></td> </tr><br/>");
 				}
 			}
 			else if( $match == "contains"){
-				$query = mysqli_query( $db, "SELECT * FROM Track WHERE ( track_name LIKE '$search_key') 
-																		AND album_type = '$type' 
-																		AND published_date >= '$from_date' 
-																		AND published_date <= '$end_date'
-																		AND published_date >= '$min_duration' 
-																		AND published_date <= '$max_duration';");
+				$query = mysqli_query( $db, "SELECT * FROM Track WHERE ( track_name LIKE '%$search_key%') 
+																		AND recording_type = '$type' 
+																		AND date_of_addition >= '$from_date'
+																		AND date_of_addition <= '$end_date';");
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='album.php?track_id=".$row['track_id']."'>.$row['track_id'].</a></td> </tr>");
+					echo( "<tr> <td><a href='album.php?track_id=".$row['track_id']."'>".$row['track_name']."</a></td> </tr><br/>");
 				}
 			}
 			else if( $match == "starts_with"){
 				$query = mysqli_query( $db, "SELECT * FROM Track WHERE ( track_name LIKE '$search_key') 
-																		AND album_type = '$type' 
-																		AND published_date >= '$from_date' 
-																		AND published_date <= '$end_date'
-																		AND published_date >= '$min_duration' 
-																		AND published_date <= '$max_duration';");
+																		AND recording_type = '$type' 
+																		AND date_of_addition >= '$from_date'
+																		AND date_of_addition <= '$end_date';");
 				while( $row = $query->fetch_assoc()){ 
-					echo( "<tr> <td><a href='album.php?track_id=".$row['track_id']."'>.$row['track_id'].</a></td> </tr>");
+					echo( "<tr> <td><a href='album.php?track_id=".$row['track_id']."'>".$row['track_id']."</a></td> </tr>");
 				}
 			}
 			
@@ -144,7 +137,10 @@ include("session.php");
 		?>
 	
 		<br/><br/><br/>
-
+		<div align = "center">
+			<tr><td><a href='logout.php'>Logout</a></td></tr>
+		</div>
+	</nav>
 
 	<div>
 	<footer>
