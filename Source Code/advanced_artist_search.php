495fa2e7386eab49461227eae49569db06dd6475
Esra Ayaz <esranayaz@gmail.com>
2018-05-14 08:28:23 +0300

    Update advanced_artist_search.php

--- Source Code/advanced_artist_search.php
+++ Source Code/advanced_artist_search.php
@@ -68,7 +68,7 @@
 			
 			
 			if( $search_key == "")
-				echo ' <script type="text/javascript"> alert("Please type an album name"); </script>';
+				echo ' <script type="text/javascript"> alert("Please type an artist name"); </script>';
 			if( $match == "")
 				echo ' <script type="text/javascript"> alert("Select from Exactly matches, Contains, Starts with "); </script>';
 			
